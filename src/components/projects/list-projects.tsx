ae9977d3e9f8ff514bd797b03d0eabd8b3db615e
SLIMANI Imed Eddine Abderrahmane <imeds2000@gmail.com>
2025-06-24 19:40:33 +0100

    remove console.log

--- src/components/projects/list-projects.tsx
+++ src/components/projects/list-projects.tsx
@@ -31,10 +31,7 @@ export default function ListProjects() {
     Empty: <></>,
     Success: ({ data }) => {
       const projects = data.data
-      console.log(
-        'test projects: ',
-        projects.map((item) => fileUrl(item.image_path))
-      )
+
       return (
         <div>
           <ul
@@ -58,8 +55,7 @@ export default function ListProjects() {
                     {project.image_path ? (
                       <Image
                         alt=""
-                        // src={fileUrl(project.image_path)!}
-                        src="https://taskflow-be-e9cwcqfrgmd9cngf.francecentral-01.azurewebsites.net/storage/workspaces/VqqqmsuBdpPf7Hevt8yPLQdZvfu2V0gGjEHyw4Yc.webp"
+                        src={fileUrl(project.image_path)!}
                         className="size-6 shrink-0 rounded-lg"
                         width={24}
                         height={24}

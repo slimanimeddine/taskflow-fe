f3934c0b34ba7f75f64baea0a0a8875c9517f393
SLIMANI Imed Eddine Abderrahmane <imeds2000@gmail.com>
2025-06-24 19:55:20 +0100

    opt for overrideSrc attribute to fix image display problem

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'images.unsplash.com',
-      // },
       {
         protocol: 'http',
         hostname: 'localhost',

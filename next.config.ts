889a7933752f9007f522bc22986da8da56050dbd
SLIMANI Imed Eddine Abderrahmane <imeds2000@gmail.com>
2025-06-25 04:59:17 +0100

    try out unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       new URL(
         'https://taskflow-be-e9cwcqfrgmd9cngf.francecentral-01.azurewebsites.net/storage/**'

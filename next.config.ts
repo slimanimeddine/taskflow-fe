a8e24dd18d6016de100373020eed613d7e063ef7
SLIMANI Imed Eddine Abderrahmane <imeds2000@gmail.com>
2025-08-14 12:45:25 +0100

    remove github workflows, ecosystem.js and remote image url

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
-      new URL(
-        'https://taskflow-be-e9cwcqfrgmd9cngf.francecentral-01.azurewebsites.net/storage/**'
-      ),
       new URL('http://localhost:8000/storage/**'),
     ],
   },

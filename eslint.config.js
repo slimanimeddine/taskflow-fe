51c273cea7d462921dbf0746a0cdce0533e9648c
Imed Eddine Abderrahmane SLIMANI <imeds2000@gmail.com>
2025-08-26 20:01:45 +0100

    fix eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   {
-    ignores: [".next"],
+    ignores: [".next", "next-env.d.ts"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
